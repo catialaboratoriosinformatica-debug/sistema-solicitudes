5c1d86dc3e5406d8c3dcf4418432b8308a540577
catialaboratoriosinformatica-debug <catialaboratoriosinformatica@iujo.edu.ve>
2026-09-24 17:01:20 -0400

    Fix entrypoint order and add user to DatabaseSeeder

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,8 @@ php artisan view:clear
 php artisan filament:assets
 php artisan storage:link --force
 
+# Ejecutar migraciones y seeders en producción
+php artisan migrate --force --seed
+
 # Iniciar el servidor web (ajusta según cómo arranques tu app)
 php artisan serve --host=0.0.0.0 --port=10000
